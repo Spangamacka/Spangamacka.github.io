d1f8b7747cfaa9c0115e7ed88c704e4cf6dd7dff
Spangamacka <165089140+Spangamacka@users.noreply.github.com>
2024-03-28 22:10:39 +0100

    small design update.

---
+++ _next/static/vQze-eDumKqTi2olsL65u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/908b297121ed8cd9.css","static/chunks/pages/index-8ee86fc8ddc26df7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
