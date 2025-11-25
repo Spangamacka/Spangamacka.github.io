f96ff59f57ffdff5dfb74f4113e15dcbe0d9c68e
Simon <s.lindgren@hotmail.se>
2025-11-25 23:23:03 +0100

    Updates

---
+++ _next/static/ZKdcwYQh2lrM7Uxim06Gl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/03e949c77b0e36da.css","static/chunks/pages/index-121d2595849f2ab3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
