961f604b86e0d357108533fe190eb41e647d48d7
Simon <s.lindgren@hotmail.se>
2025-12-01 06:12:37 +0100

    Updates

--- _next/static/qEU8lN8c3fvbsbapltjei/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1a61eb930dd9900c.css","static/chunks/pages/index-5bc2b9790555631e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/qEd1tCYN3n_NFw0gIxQ4r/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c08ed89b949bdf6c.css","static/chunks/pages/index-0b7439eabcbdf5ae.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
